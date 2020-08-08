e95209b7f387c38dfa856c80006c74f5419b0513
Yash <50296509+yash1807@users.noreply.github.com>
2020-08-08 16:26:06 +0530

    Update AspectLogging.java

--- Spring-Morning/Spring/src/main/java/com/au/spring/aspect/AspectLogging.java
+++ Spring-Morning/Spring/src/main/java/com/au/spring/aspect/AspectLogging.java
@@ -1,6 +1,5 @@
 package com.au.spring.aspect;
 
-import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
